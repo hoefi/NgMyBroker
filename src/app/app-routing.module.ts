a1b6e4c418361ca0d5de6c2a7fcf1164d6df00a5
Matthias Hoefel <matthias@hoefel.net>
2019-06-06 09:51:04 +0200

    Two pages with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { NewStockComponent } from './new-stock/new-stock.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StockViewComponent } from './stock-view/stock-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StockViewComponent },
+  { path: 'addStock', component: NewStockComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NewStockComponent } from './new-stock/new-stock.component';
+import { StockViewComponent } from './stock-view/stock-view.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewStockComponent,
+    StockViewComponent
   ],
   imports: [
     BrowserModule,
